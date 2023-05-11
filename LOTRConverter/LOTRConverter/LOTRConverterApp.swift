007fd945dade0b9c8c218f3fb085b7d9d573907e
Jeff Sweeney <Jeffrey.Sweeney812.GH@gmail.com>
2023-05-11 16:55:57 -0400

    Initialize empty SwiftUI app

---
+++ LOTRConverter/LOTRConverter/LOTRConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRConverterApp.swift
+//  LOTRConverter
+//
+//  Created by Jeffrey Sweeney on 5/11/23.
+//
+
+import SwiftUI
+
+@main
+struct LOTRConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
