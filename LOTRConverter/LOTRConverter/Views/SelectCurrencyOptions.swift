ba12c4bc0aa23857e5eb917aa411520acc138a25
Jeff Sweeney <Jeffrey.Sweeney812.GH@gmail.com>
2023-05-16 13:04:46 -0400

    Add a dynamically created currency tab

---
+++ LOTRConverter/LOTRConverter/Views/SelectCurrencyOptions.swift
@@ -0,0 +1,20 @@
+//
+//  SelectCurrencyOptions.swift
+//  LOTRConverter
+//
+//  Created by Jeffrey Sweeney on 5/16/23.
+//
+
+import SwiftUI
+
+struct SelectCurrencyOptions: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SelectCurrencyOptions_Previews: PreviewProvider {
+    static var previews: some View {
+        SelectCurrencyOptions()
+    }
+}
---
+++ LOTRConverter/LOTRConverter/Views/SelectCurrencyOptionsTab.swift
@@ -0,0 +1,46 @@
+//
+//  SelectCurrencyOptionsTab.swift
+//  LOTRConverter
+//
+//  Created by Jeffrey Sweeney on 5/15/23.
+//
+
+import SwiftUI
+
+fileprivate let colorTheme = Color(.systemBrown)
+
+struct SelectCurrencyOptionsTab: View {
+    let currencyImage: Image
+    let currencyName: String
+    
+    var body: some View {
+       ZStack(alignment: .bottom) {
+            Rectangle()
+                .foregroundColor(colorTheme)
+                .cornerRadius(22)
+                            
+            currencyImage
+                .resizable()
+                .scaledToFit()
+                .padding(.all, 5)
+            
+            Text(currencyName)
+                .background(colorTheme)
+                .font(.caption2)
+                .padding(.bottom, 5)
+        }
+        .frame(width: 100, height: 100)
+    }
+}
+
+struct SelectCurrencyOptionsTab_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            SelectCurrencyOptionsTab(currencyImage: Image("copperpenny"), currencyName: "Copper Penny")
+                .previewLayout(.sizeThatFits)
+            
+            SelectCurrencyOptionsTab(currencyImage: Image("goldpiece"), currencyName: "Gold Piece")
+                .previewLayout(.sizeThatFits)
+        }
+    }
+}
