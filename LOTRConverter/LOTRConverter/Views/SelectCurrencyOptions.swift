89ae9b9a60f85e93ac67bde597cfbc4f9af87ad8
Jeff Sweeney <Jeffrey.Sweeney812.GH@gmail.com>
2023-05-18 17:01:05 -0400

    Build support for a selected overlay and an update to what is selected on tap gesture

--- LOTRConverter/LOTRConverter/Views/SelectCurrencyOptions.swift
+++ LOTRConverter/LOTRConverter/Views/SelectCurrencyOptions.swift
@@ -9,14 +9,23 @@ import SwiftUI
 
 struct SelectCurrencyOptions: View {
     @State var gridLayout = [GridItem(), GridItem(), GridItem()]
-    @State var icons: [Currency] = [
-        .copperPenny, .silverPenny, .silverPiece, .goldPenny, .goldPiece
-    ]
+    @Binding var selectedCurrency: Currency
     
     var body: some View {
         LazyVGrid(columns: gridLayout) {
-            ForEach(icons, id: \.hashValue) { currency in
-                SelectCurrencyOptionsTab(currency: currency)
+            ForEach(0..<5) { caseIndex in
+                if Currency.allCases[caseIndex] == selectedCurrency {
+                    SelectCurrencyOptionsTab(currency: Currency.allCases[caseIndex])
+                                .overlay(RoundedRectangle(cornerRadius: 25)
+                                            .stroke(lineWidth: 3)
+                                            .opacity(0.5))
+                                .shadow(color: .black, radius: 6)
+                                .onTapGesture {
+                                    selectedCurrency = Currency.allCases[caseIndex]
+                                }
+                } else {
+                    SelectCurrencyOptionsTab(currency: Currency.allCases[caseIndex])
+                }
             }
         }
     }
@@ -24,6 +33,6 @@ struct SelectCurrencyOptions: View {
 
 struct SelectCurrencyOptions_Previews: PreviewProvider {
     static var previews: some View {
-        SelectCurrencyOptions()
+        SelectCurrencyOptions(selectedCurrency: .constant(.goldPiece))
     }
 }
